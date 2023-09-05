b3182ca1c3be9955200a262b3dde4acff13662fe
andaluciacurtis <andalucia.curtis@gmail.com>
2023-09-05 13:47:47 -0700

    Implementing the view

---
+++ js/view/Column.js
@@ -0,0 +1,18 @@
+export default class Column {
+  construvtor(id, title) {
+
+  }
+
+  static createRoot() {
+    const range = document.createRange();
+
+    range.selectNode(document.body);
+    return range.createContextualFragment(`
+    <div class="kanban__column">
+    <div class="kanban__column-title"></div>
+    <div class="kanban__column-items"></div>
+    <button class="kanban__add-item" type="button">+ Add</button>
+  </div>
+    `);
+  }
+}
